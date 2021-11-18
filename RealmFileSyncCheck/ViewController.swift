c08cff1ce5d9a1d552eb23f409eca88d8682c53e
raf-wtag <fahim.rahman@welldev.io>
2021-11-18 14:41:52 +0600

    iOS 12.1 support enabled and IBOutlets created

--- RealmFileSyncCheck/ViewController.swift
+++ RealmFileSyncCheck/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var login: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        login.setTitle("Log in", for: .normal)
     }
 
-
+    @IBAction func usernameEditingEnd(_ sender: Any) {
+        
+    }
+    
+    @IBAction func passwordEditingDidEnd(_ sender: Any) {
+        
+    }
+    @IBAction func loginButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Logged In", message: "Check the data in Realm Studio", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Okay", style: .default , handler: { _ in
+            
+        })
+        alert.addAction(alertAction)
+        self.present(alert, animated: true)
+    }
 }
 
