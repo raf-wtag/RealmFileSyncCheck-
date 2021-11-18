5f7462cbbfa34b581aa8b1c1dcafd86f2e4e7f7c
raf-wtag <fahim.rahman@welldev.io>
2021-11-18 13:31:46 +0600

    Initial Commit

---
+++ RealmFileSyncCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmFileSyncCheck
+//
+//  Created by Fahim Rahman on 18/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
